2fb5394dd79549df8b41e1aded473fe28552b020
Ameet <ameetrise@gmail.com>
2023-06-13 00:26:33 -0500

    set for ios

--- appsrc/screens/user/Profile.tsx
+++ appsrc/screens/user/Profile.tsx
@@ -1,21 +1,52 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from '../container/Container';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 import CustomText from '../../components/views/CustomText';
-import {TouchableOpacity} from 'react-native';
+import {View} from 'react-native';
 
 export default function Profile() {
-  const loginWithGoogle = (): void => {
-    console.log('Clicked');
+  const [userInfo, setUserInfo] = useState();
+  const loginWithGoogle = async (): Promise<void> => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const usrInfo = await GoogleSignin.signIn();
+      setUserInfo(usrInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('cancelled');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('no play services');
+      } else {
+        console.log('error: ', error.message);
+      }
+    }
   };
 
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'],
+      webClientId:
+        '838466258748-mc65gibfkigeosct8mlddluqlq8a2bs0.apps.googleusercontent.com',
+      offlineAccess: false,
+    });
+  }, []);
+
   return (
-    <Container headerTitle="Profile" wideSymmetrical>
-      <TouchableOpacity
-        onPress={loginWithGoogle}
-        style={{justifyContent: 'center', alignSelf: 'center'}}>
-        <CustomText color="blue">Profile</CustomText>
-      </TouchableOpacity>
+    <Container scrollable headerTitle="Profile" wideSymmetrical>
+      <View style={{paddingBottom: 48}}>
+        <GoogleSigninButton
+          onPress={() => {
+            loginWithGoogle();
+          }}
+        />
+        <CustomText>{JSON.stringify(userInfo)}</CustomText>
+      </View>
     </Container>
   );
 }
--- ios/LekhnathCCI/AppDelegate.mm
+++ ios/LekhnathCCI/AppDelegate.mm
@@ -2,16 +2,26 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+
+#import <RNGoogleSignin/RNGoogleSignin.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"LekhnathCCI";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+// AppDelegate.m
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application openURL:url options:options];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
