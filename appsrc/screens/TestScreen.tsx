98becb5485849a76a8cab57c31e1f983542a108d
Ameet <ameetrise@gmail.com>
2023-06-23 00:28:57 -0500

    socket working android

--- appsrc/screens/TestScreen.tsx
+++ appsrc/screens/TestScreen.tsx
@@ -1,260 +1,97 @@
-/* eslint-disable react-native/no-inline-styles */
-import * as React from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  SafeAreaView,
-  Animated,
-  Dimensions,
-} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import VectorIcon from '../components/VectorIcons';
+import {View, Text, Button, TextInput} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {io} from 'socket.io-client';
 
-const {width, height} = Dimensions.get('screen');
-function HomeScreen() {
-  let scaleAnimation = new Animated.Value(1);
-  let slideRight = new Animated.Value(1);
+export default function TestScreen() {
+  const URL = 'http://192.168.1.131:3000';
+  const socket = io(URL);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  type msd = {name: string; message: string};
+  const [messages, setMessages] = useState<msd[]>([]);
+  const [myMsg, setMyMsg] = useState<msd>();
 
-  let interSlideRight = slideRight.interpolate({
-    inputRange: [0, 1],
-    extrapolateLeft: 'extend',
-    outputRange: [1, 5],
-  });
-  let interSlideRight2 = slideRight.interpolate({
-    inputRange: [0, 1],
-    extrapolateLeft: 'extend',
-    outputRange: [1, 5],
-  });
+  useEffect(() => {
+    getMessages();
+    read();
+    function onConnect() {
+      console.log('connected');
+      setIsConnected(true);
+    }
 
-  let interScale = scaleAnimation.interpolate({
-    inputRange: [0, height],
-    outputRange: [0.4, 2],
-    extrapolateLeft: 'extend',
-    extrapolateRight: 'clamp',
-  });
-  const startAnimation = (): void => {
-    slideRight.addListener(val => {});
+    function onDisconnect() {
+      setIsConnected(false);
+    }
 
-    Animated.sequence([
-      Animated.parallel([
-        Animated.timing(slideRight, {
-          useNativeDriver: true,
-          toValue: 40,
-          duration: 500,
-        }),
-        Animated.timing(scaleAnimation, {
-          useNativeDriver: true,
-          toValue: 2,
-          duration: 500,
-        }),
-      ]),
-      Animated.timing(scaleAnimation, {
-        useNativeDriver: true,
-        toValue: 1,
-        duration: 500,
-      }),
-      Animated.parallel([
-        Animated.timing(slideRight, {
-          useNativeDriver: true,
-          toValue: 80,
-          duration: 500,
-        }),
-      ]),
-    ]).start();
-  };
-  return (
-    <View
-      style={{
-        backgroundColor: 'pink',
-        height: 64,
-        // justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-      }}>
-      <TouchableOpacity
-        onPress={() => {
-          startAnimation();
-        }}>
-        <Animated.View
-          style={{
-            transform: [{scale: scaleAnimation}, {translateX: slideRight}],
-            height: 64,
-            width: 64,
-            borderRadius: 32,
-            backgroundColor: 'brown',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text>Scale</Text>
-        </Animated.View>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
 
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{height: 24, width: 24, backgroundColor: 'red'}} />
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-function About() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{height: 24, width: 24, backgroundColor: 'red'}} />
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const imageScale = new Animated.Value(1);
-const imageX = new Animated.Value(0);
-
-const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
-
-function MyTabBar({
-  state,
-  descriptors,
-  navigation,
-}: {
-  state: any;
-  descriptors: any;
-  navigation: any;
-}) {
-  React.useEffect(() => {
-    Animated.timing(imageX, {
-      useNativeDriver: true,
-      toValue: 100,
-      duration: 1000,
-    }).start();
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
   }, []);
 
+  const read = (): void => {
+    socket.on('message', args => {
+      console.log(args);
+      setMessages(oldArray => [...oldArray, args]);
+    });
+  };
+
+  const getMessages = (): void => {
+    fetch(`${URL}/messages`, {}).then(response => {
+      response.json().then(data => {
+        let array: msd[] = [];
+        for (let i = 0; i < data.length; i++) {
+          array.push({name: data[i].name, message: data[i].message});
+        }
+        setMessages(array);
+      });
+    });
+  };
+
+  const post = async (): Promise<void> => {
+    let data = {
+      name: new Date().getTime().toString(),
+      message: myMsg?.message || 'default',
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data),
+    };
+    console.log(data);
+    try {
+      await fetch(`${URL}/messages`, requestOptions).then(response => {
+        console.log(response.status);
+        setMyMsg({});
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View style={{flexDirection: 'row', paddingBottom: 0}}>
-      {state.routes.map(
-        (route: {key: string | number; name: any}, index: any) => {
-          const {options} = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
+    <View style={{flex: 1}}>
+      <Text>
+        {messages.map(msg => {
+          return msg.message + '\n';
+        })}
+      </Text>
 
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            const event = navigation.emit({
-              type: 'tabPress',
-              target: route.key,
-            });
-
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name);
-            }
-
-            // console.log('previous tab index: ', state.index);
-            console.log('current', route.name);
-            console.log('current', index);
-            console.log('last', state.index);
-            // Animated.timing(imageScale, {
-            //   useNativeDriver: true,
-            //   toValue: 2,
-            //   duration: 1000,
-            // }).start();
-          };
-
-          const onLongPress = () => {
-            navigation.emit({
-              type: 'tabLongPress',
-              target: route.key,
-            });
-          };
-
-          <Text>{JSON.stringify(state)}</Text>;
-
-          return (
-            <AnimatedTouchable
-              key={route.key}
-              accessibilityRole="button"
-              accessibilityState={isFocused ? {selected: true} : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{
-                transform: [
-                  {
-                    scale: imageScale.interpolate({
-                      inputRange: [0, 1],
-                      outputRange: [1, 1],
-                    }),
-                  },
-                  {
-                    translateY: Animated.divide(
-                      imageX.interpolate({
-                        inputRange: [0, 30],
-                        outputRange: [30, 0],
-                      }),
-                      imageX.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: [1, 1.5],
-                      }),
-                    ),
-                  },
-                ],
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: isFocused ? 'orange' : 'white',
-                paddingBottom: 28,
-              }}>
-              <Text style={{color: isFocused ? '#673ab7' : '#222'}}>
-                {label}
-              </Text>
-              <VectorIcon
-                iconFamily={'AntDesign'}
-                iconName={'home'}
-                iconSize={18}
-                iconColor={isFocused ? 'blue' : 'green'}
-              />
-            </AnimatedTouchable>
-          );
-        },
-      )}
+      <TextInput
+        value={myMsg?.message}
+        placeholder="type here"
+        onChangeText={text => {
+          setMyMsg({name: 'amit', message: text});
+        }}
+      />
+      <Button
+        title="Post"
+        onPress={() => {
+          post();
+        }}
+      />
     </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
-        <Tab.Screen
-          navigationKey="a"
-          key={'1'}
-          name="Home"
-          component={HomeScreen}
-        />
-        <Tab.Screen
-          navigationKey="ss"
-          key={'b'}
-          name="Settings"
-          component={SettingsScreen}
-        />
-        <Tab.Screen
-          navigationKey="sss"
-          key={'sb'}
-          name="About"
-          component={About}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
   );
 }
